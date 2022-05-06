ca2be1eaccf29a7dbaac3cc4983c76bad2474b01
Wallisson Martins <wallissonmartins37@gmail.com>
2022-05-05 23:20:51 -0300

    first commit

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,47 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+import ChinaFlag from '../../assets/images/china.png'
+import GermanyFlag from '../../assets/images/germany.png'
+import ItalyFlag from '../../assets/images/italy.png'
+import UsaFlag from '../../assets/images/usa.png'
+import JapanFlag from'../../assets/images/japan.png'
+import PolandFlag from'../../assets/images/poland.png'
+
+const COUNTRIES = [
+  {
+    label: 'Alemanha',
+    value: 'Germany',
+    flag: GermanyFlag
+  },
+  {
+    label: 'Brasil',
+    value: 'Brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'China',
+    value: 'China',
+    flag: ChinaFlag
+  },
+  {
+    label: 'Estados Unidos',
+    value: 'USA',
+    flag: UsaFlag
+  },
+  {
+    label: 'Itália',
+    value: 'Italy',
+    flag: ItalyFlag
+  },
+  {
+    label: 'Japão',
+    value: 'Japan',
+    flag: JapanFlag
+  },
+  {
+    label: 'Polônia',
+    value: 'Poland',
+    flag: PolandFlag
+  }
+]
+
+export default COUNTRIES
---
+++ src/containers/Main/components/Panel.jsx
@@ -0,0 +1,69 @@
+import React, { memo } from 'react'
+import { Card, Typography, Button, Select, MenuItem } from '../../../components'
+import COUNTRIES from '../../../commons/constants/countries'
+import { CardPanelContentStyled, ItemStyled } from './style'
+
+const navigatorHasShare = navigator.share
+
+function Panel({ updateAt, onChange, data, country }) {
+  const { recovered } = data
+
+  const renderCountries = (country, index) => (
+    <MenuItem key={`country-${index}`} value={country.value}>
+      <ItemStyled>
+        <div>{country.label}</div>
+        <img src={country.flag} alt={`País-${country.label}`} />
+      </ItemStyled>
+    </MenuItem>
+  )
+
+  const textCovid19 = `País: ${country} - recuperados: ${recovered}`
+ 
+  const copyInfo = () => {
+    navigator.clipboard.writeText(textCovid19)
+  }
+
+  const shareInfo = () => {
+    navigator.share({
+      title: `Dados da COVID-19 - ${country}`,
+      text: textCovid19,
+      url: 'https://covid19-project.netlify.app/'
+    })
+  }
+
+  const renderShareButton = (
+    <div>
+      <Button variant="contained" color="primary" onClick={shareInfo}>
+        Compartilhar
+      </Button>
+    </div>
+  )
+
+  const renderCopyButton = (
+    <div>
+      <Button variant="contained" color="primary" onClick={copyInfo}>
+        Copiar
+      </Button>
+    </div>
+  )
+
+  return (
+    <Card>
+      <CardPanelContentStyled>
+        <div>
+          <Typography variant="h5" component="div" color="primary">COVID-19</Typography>
+          <Typography variant="h6" component="div" color="primary">Painel Coronavírus</Typography>
+          <Typography variant="subtitle2" component="div" color="primary">Atualizado em: {updateAt}</Typography>
+          <div className="pt-2">
+            <Select onChange={onChange} value={country}>
+              {COUNTRIES.map(renderCountries)}
+            </Select>
+          </div>
+        </div>
+        {navigatorHasShare ? renderShareButton : renderCopyButton}
+      </CardPanelContentStyled>
+    </Card>
+  )
+}
+
+export default memo(Panel)
